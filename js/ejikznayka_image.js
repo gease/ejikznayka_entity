5df9b87446504f51a9539c8b2fb383ecb3e0d725
Vadim Valuev <gease@mail.ru>
2018-03-12 12:57:44 +0000

    Added 4 text fields to image paragraph.

--- js/ejikznayka_image.js
+++ js/ejikznayka_image.js
@@ -6,24 +6,16 @@
         $(this).next('.overlay').css('display', 'flex');
         $(this).next('.overlay').css('font-size', settings.ejikznayka.display_settings.font_size + 'px');
         $(this).next('.overlay').children('.field--name-field-paragraph-image-based-text').addClass('ejikznayka-top ejikznayka-left');
+        $(this).next('.overlay').children('.field--name-field-paragraph-image-based-tex1').addClass('ejikznayka-bottom ejikznayka-left');
+        $(this).next('.overlay').children('.field--name-field-paragraph-image-based-tex2').addClass('ejikznayka-bottom ejikznayka-right');
+        $(this).next('.overlay').children('.field--name-field-paragraph-image-based-tex3').addClass('ejikznayka-top ejikznayka-right');
         const $self = $(this);
         setTimeout(function () {
           $self.next('.overlay').css('display', 'none');
-          console.log($self.parent('.field__item').next('.field__item').children('button.ejikznayka_page'));
           $self.parent('.field__item').next('.field__item').children('button.ejikznayka_page').click();
-        }, settings.ejikznayka.display_settings.interval * 1000);
-        //console.log(settings);
-        //$(this).next('.overlay').children('div:first-of-type').show();
+        },
+        settings.ejikznayka.display_settings.interval * 1000);
       });
-      /*$('.overlay', context).click(function () {
-        //$(this).children('div:visible').hide().next().not('.field--name-field-paragraph-image-based-soun').show();
-        $(this).children('.field--name-field-paragraph-image-based-text').addClass(['ejikznayka-top', 'ejikznayka-left']);
-        if ($(this).children('div:visible').length === 0) {
-          $(this).children('.field--name-field-paragraph-image-based-soun').find('audio').get(0).play();
-          $(this).hide();
-
-        }
-      });*/
     }
   };
 })(jQuery);
